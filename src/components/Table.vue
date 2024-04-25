d7e62a171a874ea0fda26861df7434e04154d095
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-25 21:35:25 +0700

    Unassigned

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,46 +1,49 @@
 <script setup>
-    defineProps({
-        tasks: Array
-    })
+defineProps({
+  tasks: Array,
+})
 </script>
- 
+
 <template>
-
-    <h1 class="text-center text-2xl  bg-black bg-clip-content">IT-Bangmod Kradan Kanban SSI-3</h1>
-<div>
-    <table class="border-collapse bg-white border w-full  " >
-        <thead>
-            <tr class=" text-black bg-gray-400 ">
-                <th></th>
-                <th>Title</th>
-                <th>Assignees</th>
-                <th>Status</th>
-            </tr>
-            <tr v-for="(task,index) in tasks" :class="itbkk-item" :key="index">
-                <td class=" text-black text-center" >{{ task.taskId }}</td>
-                <td class="itbkk-title text-blue-400" >{{ task.taskTitle }}</td>
-                <td class="itbkk-assignees text-black">{{ task.assignees }}</td>
-                <td class="itbkk-status bg-green-500/60 bg-left bg-clip-content rounded-full size-1  text-black">{{ task.status }}</td>
-            </tr>
-        </thead>
+  <h1 class="text-center text-2xl bg-black bg-clip-content">
+    IT-Bangmod Kradan Kanban SSI-3
+  </h1>
+  <div>
+    <table class="border-collapse bg-white border w-full">
+      <thead>
+        <tr class="text-black bg-gray-400">
+          <th></th>
+          <th>Title</th>
+          <th>Assignees</th>
+          <th>Status</th>
+        </tr>
+        <tr v-for="(task, index) in tasks" :key="index" class="itbkk-item">
+          <td class="text-black text-center">{{ task.taskId }}</td>
+          <td class="itbkk-title text-blue-400">{{ task.taskTitle }}</td>
+          <td
+            class="itbkk-assignees text-black"
+            :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }"
+          >
+            {{ task.assignees ? task.assignees : 'Unassigned' }}
+          </td>
+          <td
+            class="itbkk-status bg-green-500/60 bg-left bg-clip-content rounded-full size-1 text-black"
+          >
+            {{ task.status }}
+          </td>
+        </tr>
+      </thead>
     </table>
-</div>
+  </div>
 </template>
- 
+
 <style scoped>
-
-
 td {
-
-    padding: 24px;
-  border:3px black solid;
-
+  padding: 24px;
+  border: 3px black solid;
 }
- th {
-    padding: 12px;
-  border:3px black solid;
- }
-
-
-
-</style>    
+th {
+  padding: 12px;
+  border: 3px black solid;
+}
+</style>
