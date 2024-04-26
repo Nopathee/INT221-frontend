5d72a750292677e934ec20dc8c62cfeafbdf372f
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-26 22:07:12 +0700

    fix bug

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -27,15 +27,15 @@ defineEmits(['showDetail'])
           <th>Assignees</th>
           <th>Status</th>
         </tr>
-        <tr v-for="(task, index) in tasks" :key="index">
+        <tr v-for="(task, index) in tasks" :key="index" class="itbkk-item">
           <td class="text-black text-center">{{ task.taskId }}</td>
-          <td class="text-blue-400 hover:underline">
+          <td class="text-blue-400 hover:underline itbkk-title">
             <button @click="$emit('showDetail',task)">{{ task.taskTitle }}</button>
           </td>
-          <td class="text-black">{{ task.assignees }}</td>
+          <td class="text-black itbkk-assignees" :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }" ç>{{ task.assignees ? task.assignees : "Unassigned" }}</td>
 
           <td
-            class="bg-green-500/60 bg-left bg-clip-content rounded-full size-1 text-black"
+            class="bg-green-500/60 bg-left bg-clip-content rounded-full size-1 text-black itbkk-status"
           >
             {{ task.status }}
           </td>
