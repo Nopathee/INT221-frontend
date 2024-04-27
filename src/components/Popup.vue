a794967b33eb2de2646402516700adff62fe17f8
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-27 17:36:42 +0700

    commit

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -3,7 +3,18 @@ import { defineProps, defineEmits, ref, onMounted , computed ,  } from 'vue';
 
 defineEmits(['close'])
 const props = defineProps({
-  task: Object
+  task: {type: Object,
+        default: () => {
+      return {
+        title: '',
+        description: '',
+        assignees: '',
+        status: '',
+        createdOn: '',
+        updatedOn: ''
+      }
+    }
+  }
 })
 
 const showTask = computed(() => {
@@ -40,63 +51,50 @@ const formattedUpdatedOn = computed(() => {
   return `${dateString} ${timeString}`;
 });
 
-
-
-
 </script>
 
 <template>
   <div class=" flex flex-col p-2 gap-5">
-    <h1 class = "font-semibold text-2xl itbkk-title " > {{ showTask ? showTask.title : ''}}</h1>
+    <h1 class="font-semibold text-2xl itbkk-title">{{ showTask && showTask.title ? showTask.title : ''}}</h1>
   </div>
   <hr>
   <div class=" flex">
     <div class=" ml-2">
-      <p class=" text-sm">Desription</p>
-      <textarea class="border-2 border-black h-52 w-96 itbkk-description " >{{ showTask ? showTask.description : '' }}</textarea>
+      <p class="text-sm">Desription</p>
+      <textarea class="border-2 border-black h-52 w-96 itbkk-description">{{ showTask && showTask.description ? showTask.description : '' }}</textarea>
     </div>
-    <div class=" flex flex-col">
-      <div class=" ml-1">
-        <p class=" text-sm">Assignees</p>
-        <textarea class=" border-2 border-slate-600 h-24 w-72 itbkk-assignees" >{{ showTask ? showTask.assignees : '' }}</textarea>
+    <div class="flex flex-col">
+      <div class="ml-1">
+        <p class="text-sm">Assignees</p>
+        <textarea class="border-2 border-slate-600 h-24 w-72 itbkk-assignees">{{ showTask && showTask.assignees ? showTask.assignees : '' }}</textarea>
       </div>
-      <div class=" ml-1">
-        <p class=" text-sm">Status</p>
-        <select class=" border-2 border-slate-600 h-7 w-72 itbkk-status" placeholder="Status" >
-          <option value="">Status</option>
-          <option value="todo">To Do</option>
-          <option value="doing">Doing</option>
-          <option value="done">Done</option>
+      <div class="ml-1">
+        <p class="text-sm">Status</p>
+        <select class="border-2 border-slate-600 h-7 w-72 itbkk-status" v-model="showTask && showTask.status ? showTask.status : ''">
+          <option value="No Status">No Status</option>
+          <option value="To Do">To Do</option>
+          <option value="Doing">Doing</option>
+          <option value="Done">Done</option>
         </select>
       </div>
-      <div class=" m-1 flex">
-        <p class=" text-sm">TimeZone</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-timezone" v-model="showTime.timezone" ></input>
+      <div class="m-1 flex">
+        <p class="text-sm">TimeZone</p>
+        <input type="text" class="border-2 border-slate-600 h-5 w-56 itbkk-timezone" v-model="showTime.timezone"></input>
       </div>
-      <div class=" m-1 flex">
-        <p class=" text-sm">CreateOn</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-create-on" v-model="formattedCreateOn"></input>
+      <div class="m-1 flex">
+        <p class="text-sm">CreateOn</p>
+        <input type="text" class="border-2 border-slate-600 h-5 w-56 itbkk-create-on" v-model="formattedCreateOn"></input>
       </div>
-      <div class=" m-1 flex">
-        <p class=" text-sm">UpdatedOn</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-updated-on" v-model="formattedUpdatedOn"></input>
-        </div>
+      <div class="m-1 flex">
+        <p class="text-sm">UpdatedOn</p>
+        <input type="text" class="border-2 border-slate-600 h-5 w-56 itbkk-updated-on" v-model="formattedUpdatedOn"></input>
+      </div>
     </div>
   </div>
-  <div class=" flex justify-end gap-3 px-2">
-    <button
-      class="text-black mt-8 bg-green-400 rounded-lg py-2 px-6 text-right"
-      @click="$emit('close')"
-    >
-      Ok
-    </button>
-    <button
-      class="text-black mt-8 bg-red-500 rounded-lg py-2 px-6 text-right"
-      @click="$emit('close', false)"
-    >
-      Close
-    </button>
+  <div class="flex justify-end gap-3 px-2">
+    <button class="text-black mt-8 bg-green-400 rounded-lg py-2 px-6 text-right" @click="$emit('close')">Close</button>
   </div>
 </template>
+        
 
 <style scoped></style>
