d5cdef6013be0b8e60c2ea3cd1d0715c5d5bfef5
Aukky1 <thitiphat.tana@kmutt.ac.th>
2024-04-27 17:23:51 +0700

    Update Popup.vue

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -47,39 +47,36 @@ const formattedUpdatedOn = computed(() => {
 
 <template>
   <div class=" flex flex-col p-2 gap-5">
-    <h1 class = "font-semibold text-2xl itbkk-title " > {{ showTask ? showTask.title : ''}}</h1>
+    <h1 class = "font-semibold text-2xl itbkk-title text-black" > {{ showTask ? showTask.title : ''}}</h1>
   </div>
   <hr>
   <div class=" flex">
     <div class=" ml-2">
-      <p class=" text-sm">Desription</p>
+      <p class=" text-sm text-black">Desription</p>
       <textarea class="border-2 border-black h-52 w-96 itbkk-description " >{{ showTask ? showTask.description : '' }}</textarea>
     </div>
     <div class=" flex flex-col">
       <div class=" ml-1">
-        <p class=" text-sm">Assignees</p>
+        <p class=" text-sm text-black">Assignees</p>
         <textarea class=" border-2 border-slate-600 h-24 w-72 itbkk-assignees" >{{ showTask ? showTask.assignees : '' }}</textarea>
       </div>
       <div class=" ml-1">
-        <p class=" text-sm">Status</p>
-        <select class=" border-2 border-slate-600 h-7 w-72 itbkk-status" placeholder="Status" >
-          <option value="">Status</option>
+        <p class=" text-sm text-black">Status</p>
+        <select class=" border-2 border-slate-600 h-7 w-72 itbkk-status" v-model="showTask.status">
+          <option value="No status">No Status</option>
           <option value="todo">To Do</option>
           <option value="doing">Doing</option>
           <option value="done">Done</option>
         </select>
       </div>
       <div class=" m-1 flex">
-        <p class=" text-sm">TimeZone</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-timezone" v-model="showTime.timezone" ></input>
+        <p class=" text-sm text-black">TimeZone : {{ showTime.timezone }}</p>
       </div>
       <div class=" m-1 flex">
-        <p class=" text-sm">CreateOn</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-create-on" v-model="formattedCreateOn"></input>
+        <p class=" text-sm text-black">CreateOn : {{ formattedCreateOn }}</p>
       </div>
       <div class=" m-1 flex">
-        <p class=" text-sm">UpdatedOn</p>
-        <input type="text" class="  border-2 border-slate-600 h-5 w-56 itbkk-updated-on" v-model="formattedUpdatedOn"></input>
+        <p class=" text-sm text-black">UpdatedOn : {{ formattedUpdatedOn }}</p>
         </div>
     </div>
   </div>
