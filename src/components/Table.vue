2e75b114c521e69b3e4e0a77b59b7cb879e402ce
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-29 18:43:54 +0700

    Update Table.vue

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -5,59 +5,58 @@ defineProps({
 </script>
 
 <template>
-  <div class="w-full gap-5 p-5">
-    <h1 class="text-center text-2xl bg-clip-content p-3 font-extrabold">
-      IT-Bangmod Kradan Kanban SSI-3
-    </h1>
-    <div class="flex justify-center">
-      <table class="w-3/4 table table-lg">
+  <div class="w-full flex justify-center items-center">
+    <div class="w-3/4 rounded-xl p-5">
+      <h1 class="text-center text-2xl bg-clip-content p-3 font-extrabold">
+        IT-Bangmod Kradan Kanban SSI-3
+      </h1>
+      <table class="w-full table table-lg rounded-lg overflow-hidden">
         <thead>
-          <div class="bg-pink-300 rounded-xl w-full">
-            <tr class="font-bold text-red-800 text-lg">
-              <th></th>
-              <th>Title</th>
-              <th>Assignees</th>
-              <th>Status</th>
-            </tr>
-
-            <tr
-              v-for="(task, index) in tasks"
-              :key="index"
-              class="bg-blue-300 itbkk-item"
-            >
-              <td class="text-white text-center font-semibold">
-                {{ task.taskId }}
-              </td>
-              <td
-                class="text-blue-800 hover:underline itbkk-title font-semibold"
-              >
-                <RouterLink
-                  :to="{ name: 'taskDetail', params: { id: task.taskId } }"
-                  >{{ task.taskTitle }}</RouterLink
-                >
-              </td>
-              <td
-                class="text-white itbkk-assignees font-semibold"
-                :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }"
-              >
-                {{ task.assignees ? task.assignees : 'Unassigned' }}
-              </td>
-
-              <td>
-                <button
-                  class="badge itbkk-status w-24"
-                  :class="{
-                    'badge-success': task.status === 'Done',
-                    'badge-warning': task.status === 'Doing',
-                    'badge-info': task.status === 'To Do',
-                  }"
-                >
-                  {{ task.status }}
-                </button>
-              </td>
-            </tr>
-          </div>
+          <tr class="font-bold text-red-800 text-lg bg-pink-300">
+            <th></th>
+            <th>Title</th>
+            <th>Assignees</th>
+            <th>Status</th>
+          </tr>
         </thead>
+        <tbody>
+          <tr
+            v-for="(task, index) in tasks"
+            :key="index"
+            class="bg-blue-300 itbkk-item"
+          >
+            <td class="text-white text-center font-semibold">
+              {{ task.taskId }}
+            </td>
+            <td
+              class="text-blue-800 hover:underline itbkk-title font-semibold"
+            >
+              <RouterLink
+                :to="{ name: 'taskDetail', params: { id: task.taskId } }"
+                >{{ task.taskTitle }}</RouterLink
+              >
+            </td>
+            <td
+              class="text-blue-800 itbkk-assignees font-semibold"
+              :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }"
+            >
+              {{ task.assignees ? task.assignees : 'Unassigned' }}
+            </td>
+
+            <td>
+              <button
+                class="badge itbkk-status w-24"
+                :class="{
+                  'badge-success': task.status === 'Done',
+                  'badge-warning': task.status === 'Doing',
+                  'badge-info': task.status === 'To Do',
+                }"
+              >
+                {{ task.status }}
+              </button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   </div>
