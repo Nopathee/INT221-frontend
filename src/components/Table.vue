f0666ed1f2b6af0ae6eb0428b829faef87de9b98
nopa <110712245+Nopathee@users.noreply.github.com>
2024-05-02 09:28:21 +0700

    fix bug

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, ref } from 'vue';
+import { defineProps, ref } from 'vue'
 defineProps({
   tasks: Array,
 })
@@ -17,59 +17,56 @@ const changeFormatStatus = (status) => {
   }
 }
 
-
-const isModalOpen = ref(false);
+const isModalOpen = ref(false)
 const newTask = ref({
   title: '',
   description: '',
   assignees: '',
-  status: 'NO_STATUS'
-});
+  status: 'NO_STATUS',
+})
 
 const openModal = () => {
-  console.log("Opening modal");
-  isModalOpen.value = true;
-};
+  console.log('Opening modal')
+  isModalOpen.value = true
+}
 
 const closeModal = () => {
-  isModalOpen.value = false;
-};
+  isModalOpen.value = false
+}
 
 const saveTask = async () => {
   if (!newTask.value.title.trim()) {
-    alert('Title is required!');
-    return;
+    alert('Title is required!')
+    return
   }
 
   try {
-    const response = await axios.post('/api/tasks', newTask.value);
+    const response = await axios.post('/api/tasks', newTask.value)
 
     if (response.status === 201) {
-      alert('The task has been successfully added');
-
+      alert('The task has been successfully added')
 
       newTask.value = {
         title: '',
         description: '',
         assignees: '',
-        status: 'NO_STATUS'
-      };
+        status: 'NO_STATUS',
+      }
 
-      closeModal();
+      closeModal()
     } else {
-      alert('There was a problem adding the task');
+      alert('There was a problem adding the task')
     }
   } catch (error) {
-    console.error('Error saving task:', error);
-    alert('There was an error while saving the task');
+    console.error('Error saving task:', error)
+    alert('There was an error while saving the task')
   }
-};
+}
 
 const cancelTask = () => {
   console.log('cancel')
-  window.history.back();
-};
-
+  router.go(-1)
+}
 </script>
 
 <template>
@@ -81,78 +78,137 @@ const cancelTask = () => {
       <table class="w-full table table-lg rounded-lg overflow-hidden">
         <thead>
           <tr class="font-bold text-red-800 text-lg bg-pink-300">
-            <th><img src="./icon/InsertBtn.svg" alt="Add Task" @click="openModal" class="cursor-pointer" /></th>
+            <th>
+              <img
+                src="./icon/InsertBtn.svg"
+                alt="Add Task"
+                @click="openModal"
+                class="cursor-pointer"
+              />
+            </th>
             <th>Title</th>
             <th>Assignees</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(task, index) in tasks" :key="index" class="bg-blue-300 itbkk-item">
+          <tr
+            v-for="(task, index) in tasks"
+            :key="index"
+            class="bg-blue-300 itbkk-item"
+          >
             <td class="text-white text-center font-semibold">
               {{ task.id }}
               <div class="dropdown dropdown-top">
                 <div tabindex="0" role="button">
                   <img src="./icon/Threebtn.svg" />
                 </div>
-                <ul class="p-1 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-32">
-
+                <ul
+                  class="p-1 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-32"
+                >
                   <li><a class="text-black">Edit</a></li>
                   <li><a class="text-red-600">Delete</a></li>
                 </ul>
               </div>
             </td>
-            <td class="text-blue-800 hover:underline itbkk-title font-semibold flex justify-center">
-              <RouterLink :to="{ name: 'taskDetail', params: { id: task.id } }">{{ task.title }}</RouterLink>
+            <td
+              class="text-blue-800 hover:underline itbkk-title font-semibold flex justify-center"
+            >
+              <RouterLink
+                :to="{ name: 'taskDetail', params: { id: task.id } }"
+                >{{ task.title }}</RouterLink
+              >
             </td>
-            <td class="itbkk-assignees font-semibold" :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }"
-              :class="task.assignees ? 'text-white' : 'text-gray-500'">
+            <td
+              class="itbkk-assignees font-semibold"
+              :style="{ fontStyle: task.assignees ? 'normal' : 'italic' }"
+              :class="task.assignees ? 'text-white' : 'text-gray-500'"
+            >
               {{ task.assignees ? task.assignees : 'Unassigned' }}
             </td>
 
             <td>
-              <button class="badge itbkk-status w-24" :class="{
-                'badge-success': task.status === 'DONE',
-                'badge-warning': task.status === 'DOING',
-                'badge-info': task.status === 'TO_DO',
-              }">
+              <button
+                class="badge itbkk-status w-24"
+                :class="{
+                  'badge-success': task.status === 'DONE',
+                  'badge-warning': task.status === 'DOING',
+                  'badge-info': task.status === 'TO_DO',
+                }"
+              >
                 {{ changeFormatStatus(task.status) }}
               </button>
             </td>
           </tr>
         </tbody>
       </table>
-      <div v-if="isModalOpen" class="fixed inset-0 flex items-center justify-center z-50">
-        <div class="absolute inset-0 bg-black opacity-5"></div>
+      <div
+        v-if="isModalOpen"
+        class="fixed inset-0 flex items-center justify-center z-50"
+      >
         <div class="bg-white rounded-lg p-8 max-w-md dark:bg-gray-700">
           <button @click="closeModal" class="absolute top-0 right-0 p-2">
-            <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+            <svg
+              class="w-6 h-6 text-gray-700"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
             </svg>
           </button>
 
-          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h2 class="text-lg font-semibold text-gray-900 dark:text-white">Add New Task</h2>
+          <div
+            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600"
+          >
+            <h2 class="text-lg font-semibold text-gray-900 dark:text-white">
+              Add New Task
+            </h2>
           </div>
           <form class="p-4 md:p-5">
             <div class="grid gap-4 mb-4 grid-cols-2">
               <div class="col-span-2">
-                <label for="taskTitle"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                <input id="taskTitle" type="text" v-model="newTask.title"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"required="">
+                <label
+                  for="taskTitle"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Title</label
+                >
+                <input
+                  id="taskTitle"
+                  type="text"
+                  v-model="newTask.title"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                />
               </div>
               <div class="col-span-2 sm:col-span-1">
-                <label for="Assignees"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Assignees</label>
-                <input id="taskAssignees"type="text" v-model="newTask.assignees"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <label
+                  for="Assignees"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Assignees</label
+                >
+                <input
+                  id="taskAssignees"
+                  type="text"
+                  v-model="newTask.assignees"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                />
               </div>
               <div class="col-span-2 sm:col-span-1 dropdown dropdown-top">
-                <label for="Status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status:</label>
-                <select v-model="newTask.status"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <label
+                  for="Status"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Status:</label
+                >
+                <select
+                  v-model="newTask.status"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
                   <option value="NO_STATUS">No Status</option>
                   <option value="TO_DO">To Do</option>
                   <option value="DOING">Doing</option>
@@ -160,17 +216,32 @@ const cancelTask = () => {
                 </select>
               </div>
               <div class="col-span-2">
-                <label for="Description"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                <textarea v-model="newTask.description"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                <label
+                  for="Description"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Description</label
+                >
+                <textarea
+                  v-model="newTask.description"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
                 </textarea>
               </div>
             </div>
             <div class="flex justify-end">
-              <button @click="saveTask" class="px-4 py-2 bg-green-500 text-white rounded-md mr-2 "
-                :disabled="!newTask.title.trim()">Save</button>
-              <button @click="cancelTask" class="px-4 py-2 bg-red-500 text-white rounded-md">Cancel</button>
+              <button
+                @click="saveTask"
+                class="px-4 py-2 bg-green-500 text-white rounded-md mr-2"
+                :disabled="!newTask.title.trim()"
+              >
+                Save
+              </button>
+              <button
+                @click="cancelTask"
+                class="px-4 py-2 bg-red-500 text-white rounded-md"
+              >
+                Cancel
+              </button>
             </div>
           </form>
         </div>
