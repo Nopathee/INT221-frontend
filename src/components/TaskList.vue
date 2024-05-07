3d6974b6b9774426dcf697e4cf203c65ec2d8c40
nopa <110712245+Nopathee@users.noreply.github.com>
2024-05-07 12:01:27 +0700

    error toast

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -32,6 +32,8 @@ const confirmDelete = ref(false)
 
 const taskInsert = ref('')
 
+const errorToast = ref(false)
+
 const task = ref({
   id: undefined,
   title: '',
@@ -232,6 +234,11 @@ const confDelete = async (id) => {
     }, 3000)
     allTask.value.removeTask(id)
     confirmDelete.value = false
+  } else {
+    errorToast.value = true
+    setTimeout(() => {
+      errorToast.value = false
+    }, 3000)
   }
 }
 
@@ -270,7 +277,9 @@ const showEdit = async (id) => {
         </svg>
         <span class="sr-only">Check icon</span>
       </div>
-      <div class="ms-3 text-sm font-normal itbkk-message">The task {{ taskInsert }} is added successfully.</div>
+      <div class="ms-3 text-sm font-normal itbkk-message">
+        The task {{ taskInsert }} is added successfully.
+      </div>
       <button
         type="button"
         class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
@@ -318,13 +327,65 @@ const showEdit = async (id) => {
         </svg>
         <span class="sr-only">Error icon</span>
       </div>
-      <div class="ms-3 text-sm font-normal itbkk-message">Task has been deleted.</div>
+      <div class="ms-3 text-sm font-normal itbkk-message">
+        Task has been deleted.
+      </div>
       <button
         @click="deletedToast = false"
         type="button"
         class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
         data-dismiss-target="#toast-danger"
         aria-label="Close"
+      >
+        <span class="sr-only">Close</span>
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
+    <div
+      v-if="errorToast"
+      id="toast-warning"
+      class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+      role="alert"
+    >
+      <div
+        class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200"
+      >
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z"
+          />
+        </svg>
+        <span class="sr-only">Warning icon</span>
+      </div>
+      <div class="ms-3 text-sm font-normal">
+        An error occurred deleting the task.
+      </div>
+      <button
+        type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+        data-dismiss-target="#toast-warning"
+        aria-label="Close"
+        @click="errorToast = false"
       >
         <span class="sr-only">Close</span>
         <svg
