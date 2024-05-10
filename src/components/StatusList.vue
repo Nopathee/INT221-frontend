5c3d91f882d7010348ad1814e6212e62ea3880f5
KiNXS1VA <pakin.bunt@mail.kmutt.ac.th>
2024-05-10 14:50:36 +0700

    add eitd

--- src/components/StatusList.vue
+++ src/components/StatusList.vue
@@ -10,7 +10,41 @@ defineProps({
   statuses: Array,
 })
 
+const taskStatus = ref({
+  statusName: '',
+  description: ''
+})
+
 const showModalStatus = ref(false)
+const showAdd= async (id) => {
+  console.log(id)
+  router.push(`/task/${id}`)
+  const detail = await getItemById(
+    `${import.meta.env.VITE_API_ENDPOINT}/v2/tasks`,
+    id
+  )
+  console.log(detail.status)
+
+  taskStatus.value = await detail.item
+  showModalStatus.value = true
+}
+
+const editStatus = ref(false)
+const showEdit = async (id) => {
+  router.push(`/task/${id}/edit`)
+  const detail = await getItemById(
+    `${import.meta.env.VITE_API_ENDPOINT}/v2/tasks`,
+    id
+  )
+  console.log(detail.item)
+  task.value = await detail.item
+  showModalStatus.value = true
+  
+}
+
+
+
+
 const statuses = ref(new StatusManagement())
 onMounted(async () => {
   const items = await getItems(
@@ -29,97 +63,98 @@ const openModal = () => {
 
 const clearModal = () => {
   showModalStatus.value = false
+  editStatus.value = false
 }
 
+const editModal = async () => {
+
+  editStatus.value = true
+
+}
 </script>
 
 <template>
   <h1 class="  text-center text-2xl bg-clip-content p-3 font-extrabold mt-5">
-        IT-Bangmod Kradan Kanban SSI-3
-    </h1>
-    <div class=" flex justify-center">
-  <div class=" flex justify-center items-center">
-    <div class="rounded-xl p-5">
-      
-      <div class="text-left flex items-center justify-between">
-        <div class="flex items-center">
-          <h1
-            @click="back"
-            class="text-2xl font-bold text-blue-500 mr-2 hover:underline"
-          >
-            HOME
-          </h1>
-          <span class="mr-2"> > </span>
-          <h1>Task Status</h1>
+    IT-Bangmod Kradan Kanban SSI-3
+  </h1>
+  <div class=" flex justify-center">
+    <div class=" flex justify-center items-center">
+      <div class="rounded-xl p-5">
+
+        <div class="text-left flex items-center justify-between">
+          <div class="flex items-center">
+            <h1 @click="back" class="text-2xl font-bold text-blue-500 mr-2 hover:underline">
+              HOME
+            </h1>
+            <span class="mr-2"> > </span>
+            <h1>Task Status</h1>
+          </div>
+          <button
+            class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            @click="openModal">
+            Add Status
+          </button>
         </div>
-        <button
-          class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-          @click="openModal"
-        >
-          Add Status
-        </button>
+
+        <table class="w-full table table-lg rounded-lg overflow-hidden mt-5">
+          <thead>
+            <tr class="font-bold text-red-800 text-lg bg-pink-300">
+              <th></th>
+              <th>Name</th>
+              <th>Desciption</th>
+              <th class=" text-center">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(status, index) in statuses.getStatuses()" :key="index" class="bg-blue-300 itbkk-item">
+              <td class="text-white text-center font-semibold">
+                {{ index + 1 }}
+                <div class="dropdown dropdown-right dropdown-end"></div>
+              </td>
+
+              <td
+                class="text-blue-800 hover:underline itbkk-status-name font-semibold transition-property: transition-property: transform;">
+                {{ status.name }}
+              </td>
+
+              <td class="itbkk-status-description font-semibold text-white">
+                {{ status.description }}
+              </td>
+
+              <td class=" text-center">
+                <button @click="editStatus" class="btn bg-slate-200 text-black itbkk-button-edit mr-2"
+                  v-if="index !== 0">
+                  Edit
+                </button>
+
+                <button @click="$emit('deleteStatus', status.id)" class="btn btn-error itbkk-button-delete ml-2 "
+                  v-if="index !== 0">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-      <table
-        class="w-full table table-lg rounded-lg overflow-hidden mt-5"  
-      >
-        <thead>
-          <tr class="font-bold text-red-800 text-lg bg-pink-300">
-            <th></th>
-            <th>Name</th>
-            <th>Desciption</th>
-            <th class=" text-center">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(status, index) in statuses.getStatuses()"
-            :key="index"
-            class="bg-blue-300 itbkk-item"
-          >
-            <td class="text-white text-center font-semibold">
-              {{ index + 1 }}
-              <div class="dropdown dropdown-right dropdown-end"></div>
-            </td>
-
-            <td
-              class="text-blue-800 hover:underline itbkk-status-name font-semibold transition-property: transition-property: transform;"
-            >
-              {{ status.name }}
-            </td>
-
-            <td class="itbkk-status-description font-semibold text-white">
-              {{ status.description }}
-            </td>
-
-            <td class=" text-center" >
-              <button
-                @click="$emit('editStatus', status.id)"
-                class="btn bg-slate-200 text-black itbkk-button-edit mr-2"
-                v-if="index !== 0"
-              >
-                Edit
-              </button>
-              <button
-                @click="$emit('deleteStatus', status.id)"
-                class="btn btn-error itbkk-button-delete ml-2 "
-                v-if="index !== 0"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   </div>
-  </div>
+
+  <Table
+      :tasks="allTask.getTasks()"
+      @editTask="showEdit"
+    />
 
   <Teleport to="#modal">
     <div v-if="showModalStatus">
-        <StatusModal @cancelTask="clearModal" @saveTask="saveTask"  />
-      </div>
+      <StatusModal @cancelTask="clearModal" @saveTask="saveTask" />
+    </div>
   </Teleport>
+  <Teleport to="#modal">
+    <div v-if="editStatus">
+      <StatusModal @cancelTask="clearModal" @saveTask="saveTask" />
+    </div>
+  </Teleport>
+
 </template>
 
 <style scoped></style>
--- src/components/StatusModal.vue
+++ src/components/StatusModal.vue
@@ -1,26 +1,48 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { defineProps, onMounted, ref, computed } from 'vue'
 import { StatusManagement } from '../libs/StatusManagement.js'
 import { getItems } from '@/libs/fetchUtils'
+// const props = defineProps({
+//     task: {
+//         type: Object,
+//         default: () => ({
+//             statusName: '',
+//             description: ''
+//         }),
+//     },
+// })
 const props = defineProps({
-    task: {
-        type: Object,
-        default: () => ({
-            statusName: '',
-            description: ''
-        }),
-    },
+  task: Object,
 })
+onMounted(() => {
+  getTimezone()
+})
+const showTime = ref({
+  timezone: '',
+})
+const formatterCreateOn = computed(() => {
+  if (!props.task || !props.task.createdOn) return ''
+  const createdOn = new Date(props.task.createdOn).toLocaleString('en-GB')
+  return createdOn
+})
+const formatterUpdatedOn = computed(() => {
+  if (!props.task || !props.task.updatedOn) return ''
+  const updatedOn = new Date(props.task.updatedOn).toLocaleString('en-GB')
+  return updatedOn
+})
+const getTimezone = () => {
+  showTime.value.timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+}
 
 
 const statuses = ref(new StatusManagement())
 
 onMounted(async () => {
-    const items = await getItems(
-        `${import.meta.env.VITE_API_ENDPOINT}/v2/statuses`
-    )
-    statuses.value.addStatuses(items)
-    console.log(statuses.value.getStatuses())
+  const items = await getItems(
+    `${import.meta.env.VITE_API_ENDPOINT}/v2/statuses`
+  )
+  statuses.value.addStatuses(items)
+  console.log(statuses.value.getStatuses())
 })
 
 console.log(props.task)
@@ -29,41 +51,86 @@ defineEmits(['saveTask', 'cancelTask'])
 </script>
 
 <template>
-    <div>
-        <div class="fixed inset-0 flex items-center justify-center z-50">
-            <div class="bg-white rounded-lg p-8 max-w-md shadow-2xl dark:bg-gray-700">
-                <form class="p-4 md:p-5">
-                    <div class="grid gap-4 mb-4 grid-cols-2">
-                        <div class="col-span-10">
-                            <label for="Description"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">statusName</label>
-                            <input id="taskTitle" type="text" v-model.trim="props.task.statusName" maxlength="100"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-title" />
-                        </div>
-                        <div class="row-span-5 col-span-10 ">
-                            <label for="Description"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                            <textarea id="taskDescription" type="text" v-model.trim="props.task.description"
-                                maxlength="500"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-description">
-                            </textarea>
-                        </div>
-                    </div>
-                </form>
-                <div class="flex justify-end">
-                    <button @click="$emit('saveTask', props.task)"
-                        class="px-4 py-2 bg-green-500 text-white rounded-md mr-2 disabled:opacity-50 itbkk-button-confirm"
-                        :disabled="!props.task.statusName.trim()">
-                        Save
-                    </button>
-                    <button @click="$emit('cancelTask')"
-                        class="px-4 py-2 bg-red-500 text-white rounded-md itbkk-button-cancel">
-                        Cancel
-                    </button>
-                </div>
+  <div>
+    <div class="fixed inset-0 flex items-center justify-center z-50">
+      <div class="bg-white rounded-lg p-8 max-w-md shadow-2xl dark:bg-gray-700">
+        <form class="p-4 md:p-5">
+          <div class="grid gap-4 mb-4 grid-cols-2">
+            <div class="col-span-10">
+              <label for="Description"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">statusName</label>
+              <input id="taskTitle" type="text" v-model.trim="props.task.statusName" maxlength="100"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-title" />
             </div>
+            <div class="row-span-5 col-span-10 ">
+              <label for="Description"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+              <textarea id="taskDescription" type="text" v-model.trim="props.task.description" maxlength="500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-description">
+                            </textarea>
+            </div>
+          </div>
+        </form>
+        <div class="flex justify-end">
+          <button @click="$emit('saveTask', props.task)"
+            class="px-4 py-2 bg-green-500 text-white rounded-md mr-2 disabled:opacity-50 itbkk-button-confirm"
+            :disabled="!props.task.statusName.trim()">
+            Save
+          </button>
+          <button @click="$emit('cancelTask')" class="px-4 py-2 bg-red-500 text-white rounded-md itbkk-button-cancel">
+            Cancel
+          </button>
         </div>
+      </div>
     </div>
+  </div>
+
+  <div>
+    <div>
+      <div class="fixed inset-0 flex items-center justify-center z-50">
+        <div class="bg-white rounded-lg p-8 w-3/5 shadow-2xl dark:bg-gray-700">
+          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h2 class="text-lg font-semibold text-gray-900 dark:text-white">
+              Edit Status
+            </h2>
+          </div>
+          <form class="p-4 md:p-5">
+            <div class="grid gap-4 mb-4">
+              <div class="col-span-2">
+                <label for="taskTitle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+
+                <input id="taskTitle" type="text" maxlength="100"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-title" />
+              </div>
+              <div class="col-span-2">
+                <label for="Description"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+
+                <textarea id="taskDescription" type="text" maxlength="500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-description">
+              </textarea>
+              </div>
+            </div>
+          </form>
+          <div class="grid grid-rows-3 grid-flow-col gap-4  justify-between mx-2">
+            <div class="row-start-1 row-end-4">TimeZone: {{ showTime.timezone }}</div>
+            <div class="row-start-1 row-end-4">Created On: {{ formatterCreateOn }} </div>
+            <div class="row-start-1 row-end-4">Updated On: {{ formatterUpdatedOn }}</div>
+          </div>
+          <div class="flex justify-end">
+            <button @click="$emit('saveTask')"
+              class="px-4 py-2 bg-green-500 text-white rounded-md mr-2 disabled:opacity-50 itbkk-button-confirm">
+              Save
+            </button>
+            <button @click="$emit('cancelTask')" class="px-4 py-2 bg-red-500 text-white rounded-md itbkk-button-cancel">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped></style>
