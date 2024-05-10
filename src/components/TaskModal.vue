63d47bd4f050d3387c9aeb438ceddd657d839ca9
nopa <110712245+Nopathee@users.noreply.github.com>
2024-05-10 19:02:46 +0700

    change id

--- src/components/TaskModal.vue
+++ src/components/TaskModal.vue
@@ -96,7 +96,7 @@ defineEmits(['saveTask', 'cancelTask'])
                 >Status:</label
               >
               <select
-                v-model="props.task.status"
+                v-model="props.task.status.id"
                 class="itbkk-status bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 itbkk-status"
               >
                 <option value="" disabled>Select Status</option>
