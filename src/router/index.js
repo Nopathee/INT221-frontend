dc6ef766dd5a88d561737599f2b96a9d562b3014
Aukky1 <thitiphat.tana@kmutt.ac.th>
2024-05-11 19:53:34 +0700

    vite build

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Task from '../views/Task.vue'
 import { getItemById } from '@/libs/fetchUtils.js'
 import StatusList from '@/components/StatusList.vue'
-const history = createWebHistory()
+const history = createWebHistory(import.meta.env.VITE_BASE_URL)
 
 const router = createRouter({
   history,
